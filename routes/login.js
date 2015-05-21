ca7a4cbfdabcee23c02062914a7800e68765cf6f
Pascal Birchler <hello@pascalbirchler.ch>
2015-05-21 09:33:21 +0200

    Error when logging in with unknown username

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,18 @@
 var express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    userController = require('../controllers/user');
 
 router.get('/', function (req, res) {
   if (req.session) {
-    res.json({username: req.session.username});
+    res.json({username: req.session.username, fullname: req.session.fullname});
   } else {
     return null;
   }
 });
 
 router.post('/', function (req, res, next) {
-  if (!req.body.name) {
-    return next(new Error('Whoops!'));
+  if (!req.body.name || !userController.getOne(req.body.name)) {
+    return next(new Error('User does not exist!'));
   }
 
   req.session.username = req.body.name;
